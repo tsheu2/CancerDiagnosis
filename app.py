545df2dd0011b285f4e5104333e02782934ab445
TJ.Sheu <TJ.Sheu@du.edu>
2025-11-04 19:13:19 -0700

    Initial Commit

---
+++ app.py
@@ -0,0 +1,8 @@
+'''
+Naive Bayes Classification for Cancer Diagnosis
+
+Objective: To predict the most likely cancer type for a patient based on tumor marker levels
+    using the Naive Bayes classifier and Gaussian probability distributions.
+'''
+
+
